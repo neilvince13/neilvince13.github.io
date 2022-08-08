ccc39d30a0e8c35799113cd3de14e223db60d146
neilvince13 <neil.vince13@gmail.com>
2022-08-08 11:49:20 +0800

    https://neilvince13.github.io/

--- IND/GetTDBData.js
+++ IND/GetTDBData.js
@@ -43,7 +43,7 @@
     // Download the data
     myConnector.getData = function(table, doneCallback) {		
 		$.ajax({
-		url: "https://github.io/dev.thedatabase.net/api/accounts/27277/tables/7936/records",
+		url: "https://neilvince13.github.io/dev.thedatabase.net/api/accounts/27277/tables/7936/records",
 		type: "GET",
 		dataType: "json",
 		beforeSend: function (xhr){
