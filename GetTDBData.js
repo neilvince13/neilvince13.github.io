d69d3c7c8bba9f72f31ac39d170258dc23aad039
neilvince13 <neil.vince13@gmail.com>
2022-08-08 13:54:15 +0800

    Update GetTDBData.js

--- GetTDBData.js
+++ GetTDBData.js
@@ -2,14 +2,6 @@
     // Create the connector object
     var myConnector = tableau.makeConnector();
 	
-	// Init function for connector, called during every phase
-	myConnector.init = function(initCallback) {
-		tableau.authType = tableau.authTypeEnum.basic;
-		tableau.username = 'tableautraining@dbgurus.com.au';
-		tableau.password = 'dbg2022';
-		initCallback();
-	}
-	
     // Define the schema
     myConnector.getSchema = function(schemaCallback) {
         var cols = [{
